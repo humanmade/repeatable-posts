db71b48c683a3dd7f64efbe90367ab7564589623
Tom Willmot <tom@humanmade.co.uk>
2015-10-01 16:25:42 +0100

    Improved plugin meta

--- hm-post-repeat.php
+++ hm-post-repeat.php
@@ -1,11 +1,33 @@
 <?php
 
 /*
-Plugin Name: Repeating Posts
-Description: Auto duplicate and re-publish a posts at a set interval
+Plugin Name: Repeatable Posts
+Description: Designate a post as repeatable and it'll be copied and re-published on a weekly basis.
 Author: Human Made Limited
 Author URI: http://hmn.md/
 Version: 1.0
+License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+Text Domain: hm-post-repeat
+Domain Path: /languages
+*/
+
+/*
+Copyright Human Made Limited  (email : hello@hmn.md)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 define( 'HM_POST_REPEAT_PLUGIN_FILE', __FILE__ );
